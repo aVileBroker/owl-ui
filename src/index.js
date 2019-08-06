907a3e3d1b9771fd56cd6698ecd8cf3ad919333d
Oliver Baker <avilebroker@gmail.com>
2019-08-06 13:58:24 -0500

    More cleanup and adding the industryFont function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-import Colors from './utils/Colors';
-
 export Colors from './utils/Colors';
-
-export const ExampleComponent = ({ text }) => (
-  <div style={{
-    backgroundColor: Colors.dallas.primary,
-    color: Colors.dallas.secondary
-  }}>
-    Example Component: {text}
-  </div>
-);
+export industryFont from './utils/IndustryFont';
